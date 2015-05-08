cb3ffdfb0d287e5a21f122a297c27fc4cdb8cae1
Dewayne Richardson <dewrich@gmail.com>
2015-05-08 13:46:30 -0600

    created the CacheStats backend and refactor builders accordingly

--- traffic_ops/app/lib/API/v12/CacheStats.pm
+++ traffic_ops/app/lib/API/v12/CacheStats.pm
@@ -1,4 +1,4 @@
-package API::v12::DeliveryServiceStats;
+package API::v12::CacheStats;
 #
 # Copyright 2015 Comcast Cable Communications Management, LLC
 #
@@ -20,9 +20,9 @@ package API::v12::DeliveryServiceStats;
 # JvD Note: you always want to put Utils as the first use. Sh*t don't work if it's after the Mojo lines.
 use UI::Utils;
 use Mojo::Base 'Mojolicious::Controller';
-use Builder::InfluxdbBuilder;
 use Data::Dumper;
-use Builder::DeliveryServiceStatsBuilder;
+use Builder::InfluxdbBuilder;
+use Builder::CacheStatsBuilder;
 use JSON;
 my $builder;
 use constant SUCCESS => 0;
@@ -30,66 +30,52 @@ use constant ERROR   => 1;
 
 sub index {
 	my $self        = shift;
-	my $ds_name     = $self->param('deliveryServiceName');
+	my $cdn_name    = $self->param('cdnName');
 	my $metric_type = $self->param('metricType');
 	my $server_type = $self->param('serverType');
 	my $start_date  = $self->param('startDate');
 	my $end_date    = $self->param('endDate');
-	my $interval    = $self->param('interval') || "60s";     # Valid interval examples 10m (minutes), 10s (seconds), 1h (hour)
+	my $interval    = $self->param('interval') || "60s";    # Valid interval examples 10m (minutes), 10s (seconds), 1h (hour)
 	my $exclude     = $self->param('exclude');
 	my $limit       = $self->param('limit');
 	my $offset      = $self->param('offset');
 
-	if ( $self->is_valid_delivery_service_name($ds_name) ) {
-		if ( $self->is_delivery_service_name_assigned($ds_name) ) {
+	# Build the summary section
+	$builder = new Builder::CacheStatsBuilder(
+		{
+			series_name => $metric_type,
+			cdn_name    => $cdn_name,
+			start_date  => $start_date,
+			end_date    => $end_date,
+			interval    => $interval
+		}
+	);
 
-			# Build the summary section
-			$builder = new Builder::DeliveryServiceStatsBuilder(
-				{
-					series_name => $metric_type,
-					ds_name     => $ds_name,
-					start_date  => $start_date,
-					end_date    => $end_date,
-					interval    => $interval
-				}
-			);
+	my $rc     = 0;
+	my $result = ();
+	my $summary_query;
 
-			my $rc     = 0;
-			my $result = ();
-			my $summary_query;
+	my $include_summary = ( defined($exclude) && $exclude =~ /summary/ ) ? 0 : 1;
+	if ($include_summary) {
+		( $rc, $result, $summary_query ) = $self->build_summary($result);
+	}
 
-			my $include_summary = ( defined($exclude) && $exclude =~ /summary/ ) ? 0 : 1;
-			if ($include_summary) {
-				( $rc, $result, $summary_query ) = $self->build_summary($result);
-				$self->app->log->debug( "summary_query #-> " . $summary_query );
-			}
-
-			if ( $rc == SUCCESS ) {
-				my $include_series = ( defined($exclude) && $exclude =~ /series/ ) ? 0 : 1;
-				my $series_query;
-				if ($include_series) {
-					( $rc, $result, $series_query ) = $self->build_series($result);
-					$self->app->log->debug( "series_query #-> " . $series_query );
-				}
-				if ( $rc == SUCCESS ) {
-					$result = $self->build_parameters( $result, $summary_query, $series_query );
-					return $self->success($result);
-				}
-				else {
-					return $self->alert($result);
-				}
-			}
-			else {
-				return $self->alert($result);
-			}
-
+	if ( $rc == SUCCESS ) {
+		my $include_series = ( defined($exclude) && $exclude =~ /series/ ) ? 0 : 1;
+		my $series_query;
+		if ($include_series) {
+			( $rc, $result, $series_query ) = $self->build_series($result);
+		}
+		if ( $rc == SUCCESS ) {
+			$result = $self->build_parameters( $result, $summary_query, $series_query );
+			return $self->success($result);
 		}
 		else {
-			return $self->forbidden();
+			return $self->alert($result);
 		}
 	}
 	else {
-		$self->success( {} );
+		return $self->alert($result);
 	}
 
 }
@@ -99,6 +85,7 @@ sub build_summary {
 	my $result = shift;
 
 	my $summary_query = $builder->summary_query();
+	$self->app->log->debug( "summary_query #-> " . $summary_query );
 
 	my $response_container = $self->influxdb_query( $self->get_db_name(), $summary_query );
 	my $response           = $response_container->{'response'};
@@ -122,7 +109,8 @@ sub build_series {
 	my $self   = shift;
 	my $result = shift;
 
-	my $series_query       = $builder->series_query();
+	my $series_query = $builder->series_query();
+	$self->app->log->debug( "series_query #-> " . $series_query );
 	my $response_container = $self->influxdb_query( $self->get_db_name(), $series_query );
 	my $response           = $response_container->{'response'};
 	my $content            = $response->{_content};
@@ -152,23 +140,23 @@ sub build_parameters {
 	my $summary_query = shift;
 	my $series_query  = shift;
 
-	my $ds_name     = $self->param('deliveryServiceName');
+	my $cdn_name    = $self->param('cdnName');
 	my $metric_type = $self->param('metricType');
 	my $server_type = $self->param('serverType');
 	my $start_date  = $self->param('startDate');
 	my $end_date    = $self->param('endDate');
-	my $interval    = $self->param('interval') || "1m";      # Valid interval examples 10m (minutes), 10s (seconds), 1h (hour)
+	my $interval    = $self->param('interval') || "1m";    # Valid interval examples 10m (minutes), 10s (seconds), 1h (hour)
 	my $exclude     = $self->param('exclude');
 	my $limit       = $self->param('limit');
 	my $offset      = $self->param('offset');
 
 	my $parent_node     = "query";
 	my $parameters_node = "parameters";
-	$result->{$parent_node}{$parameters_node}{deliveryServiceName} = $ds_name;
-	$result->{$parent_node}{$parameters_node}{startDate}           = $start_date;
-	$result->{$parent_node}{$parameters_node}{endDate}             = $end_date;
-	$result->{$parent_node}{$parameters_node}{interval}            = $interval;
-	$result->{$parent_node}{$parameters_node}{metricType}          = $metric_type;
+	$result->{$parent_node}{$parameters_node}{cdnName}    = $cdn_name;
+	$result->{$parent_node}{$parameters_node}{startDate}  = $start_date;
+	$result->{$parent_node}{$parameters_node}{endDate}    = $end_date;
+	$result->{$parent_node}{$parameters_node}{interval}   = $interval;
+	$result->{$parent_node}{$parameters_node}{metricType} = $metric_type;
 
 	my $queries_node = "language";
 	$result->{$parent_node}{$queries_node}{influxdbDatabaseName} = $self->get_db_name();
@@ -183,7 +171,7 @@ sub get_db_name {
 	my $mode      = $self->app->mode;
 	my $conf_file = MojoPlugins::InfluxDB->INFLUXDB_CONF_FILE_NAME;
 	my $conf      = Utils::JsonConfig->load_conf( $mode, $conf_file );
-	return $conf->{deliveryservice_stats_db_name};
+	return $conf->{cache_stats_db_name};
 }
 
 1;
