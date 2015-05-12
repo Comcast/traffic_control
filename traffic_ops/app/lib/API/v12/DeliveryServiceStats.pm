93e2742cbae6306e36c241f6f1dcc064fbaf0712
Dewayne Richardson <dewrich@gmail.com>
2015-05-12 08:41:23 -0600

    fixed influx query to use the >= time and updated query to use the 'total' cachegroup

--- traffic_ops/app/lib/API/v12/DeliveryServiceStats.pm
+++ traffic_ops/app/lib/API/v12/DeliveryServiceStats.pm
@@ -29,6 +29,11 @@ Utils::Helper::Extensions->use;
 use Common::ReturnCodes qw(SUCCESS ERROR);
 
 my $builder;
+<<<<<<< HEAD
+=======
+use constant SUCCESS => 1;
+use constant ERROR   => 0;
+>>>>>>> fixed influx query to use the >= time and updated query to use the 'total' cachegroup
 
 #TODO: drichardson
 #      - Add required fields validation see lib/API/User.pm based on Validate::Tiny
@@ -39,11 +44,49 @@ sub index {
 	if ( $self->is_valid_delivery_service_name($ds_name) ) {
 		if ( $self->is_delivery_service_name_assigned($ds_name) ) {
 
+<<<<<<< HEAD
 			my $stats = new Extensions::Delegate::Statistics( $self, $self->get_db_name() );
 
 			my ( $rc, $result ) = $stats->get_stats();
 			if ( $rc == SUCCESS ) {
 				return $self->success($result);
+=======
+			# Build the summary section
+			$builder = new Builder::DeliveryServiceStatsBuilder(
+				{
+					series_name => $metric_type,
+					ds_name     => $ds_name,
+					start_date  => $start_date,
+					end_date    => $end_date,
+					interval    => $interval
+				}
+			);
+
+			my $rc     = SUCCESS;
+			my $result = ();
+			my $summary_query;
+
+			my $include_summary = ( defined($exclude) && $exclude =~ /summary/ ) ? 0 : 1;
+			if ($include_summary) {
+				( $rc, $result, $summary_query ) = $self->build_summary($result);
+				$self->app->log->debug( "summary_query #-> " . $summary_query );
+			}
+
+			if ($rc) {
+				my $include_series = ( defined($exclude) && $exclude =~ /series/ ) ? 0 : 1;
+				my $series_query;
+				if ($include_series) {
+					( $rc, $result, $series_query ) = $self->build_series($result);
+					$self->app->log->debug( "series_query #-> " . $series_query );
+				}
+				if ($rc) {
+					$result = $self->build_parameters( $result, $summary_query, $series_query );
+					return $self->success($result);
+				}
+				else {
+					return $self->alert($result);
+				}
+>>>>>>> fixed influx query to use the >= time and updated query to use the 'total' cachegroup
 			}
 			else {
 				return $self->alert($result);
@@ -59,6 +102,93 @@ sub index {
 
 }
 
+<<<<<<< HEAD
+=======
+sub build_summary {
+	my $self   = shift;
+	my $result = shift;
+
+	my $summary_query = $builder->summary_query();
+
+	my $response_container = $self->influxdb_query( $self->get_db_name(), $summary_query );
+	my $response           = $response_container->{'response'};
+	my $content            = $response->{_content};
+
+	my $summary;
+	my $summary_content;
+	my $series_count = 0;
+	if ( $response->is_success() ) {
+		$summary_content   = decode_json($content);
+		$summary           = $builder->summary_response($summary_content);
+		$result->{summary} = $summary;
+		return ( SUCCESS, $result, $summary_query );
+	}
+	else {
+		return ( ERROR, $content, undef );
+	}
+}
+
+sub build_series {
+	my $self   = shift;
+	my $result = shift;
+
+	my $series_query       = $builder->series_query();
+	my $response_container = $self->influxdb_query( $self->get_db_name(), $series_query, "pretty" );
+	my $response           = $response_container->{'response'};
+	my $content            = $response->{_content};
+
+	my $series;
+	if ( $response->is_success() ) {
+		my $series_content = decode_json($content);
+		$series = $builder->series_response($series_content);
+		my $series_node = "series";
+		if ( defined($series) && ( keys $series ) ) {
+			$result->{$series_node} = $series;
+			my @series_values = $series->{values};
+			my $series_count  = $#{ $series_values[0] };
+			$result->{$series_node}{count} = $series_count;
+		}
+		return ( SUCCESS, $result, $series_query );
+	}
+
+	else {
+		return ( ERROR, $content, undef );
+	}
+}
+
+sub build_parameters {
+	my $self          = shift;
+	my $result        = shift;
+	my $summary_query = shift;
+	my $series_query  = shift;
+
+	my $ds_name     = $self->param('deliveryServiceName');
+	my $metric_type = $self->param('metricType');
+	my $server_type = $self->param('serverType');
+	my $start_date  = $self->param('startDate');
+	my $end_date    = $self->param('endDate');
+	my $interval    = $self->param('interval') || "1m";      # Valid interval examples 10m (minutes), 10s (seconds), 1h (hour)
+	my $exclude     = $self->param('exclude');
+	my $limit       = $self->param('limit');
+	my $offset      = $self->param('offset');
+
+	my $parent_node     = "query";
+	my $parameters_node = "parameters";
+	$result->{$parent_node}{$parameters_node}{deliveryServiceName} = $ds_name;
+	$result->{$parent_node}{$parameters_node}{startDate}           = $start_date;
+	$result->{$parent_node}{$parameters_node}{endDate}             = $end_date;
+	$result->{$parent_node}{$parameters_node}{interval}            = $interval;
+	$result->{$parent_node}{$parameters_node}{metricType}          = $metric_type;
+
+	my $queries_node = "language";
+	$result->{$parent_node}{$queries_node}{influxdbDatabaseName} = $self->get_db_name();
+	$result->{$parent_node}{$queries_node}{influxdbSeriesQuery}  = $series_query;
+	$result->{$parent_node}{$queries_node}{influxdbSummaryQuery} = $summary_query;
+
+	return $result;
+}
+
+>>>>>>> fixed influx query to use the >= time and updated query to use the 'total' cachegroup
 sub get_db_name {
 	my $self      = shift;
 	my $mode      = $self->app->mode;
