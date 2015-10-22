f7aba9475aa90fec2fb7a2cd4c6e85ee374970b0
Trevor Ackerman <trevorackerman.softdev@gmail.com>
2015-10-22 11:13:34 -0600

    Don't dump out stack traces when clients send malformed dns requests, just log a single line

--- traffic_router/core/src/main/java/com/comcast/cdn/traffic_control/traffic_router/core/dns/protocol/TCP.java
+++ traffic_router/core/src/main/java/com/comcast/cdn/traffic_control/traffic_router/core/dns/protocol/TCP.java
@@ -25,6 +25,7 @@ import java.net.Socket;
 
 import org.apache.log4j.Logger;
 import org.xbill.DNS.Message;
+import org.xbill.DNS.WireParseException;
 
 public class TCP extends AbstractProtocol {
     private static final Logger LOGGER = Logger.getLogger(TCP.class);
@@ -49,7 +50,7 @@ public class TCP extends AbstractProtocol {
                 submit(handler);
             } catch (final IOException e) {
 				LOGGER.warn("error: " + e);
-				LOGGER.debug(e,e);
+				LOGGER.debug(e, e);
             }
         }
     }
@@ -99,6 +100,8 @@ public class TCP extends AbstractProtocol {
                 final byte[] response = query(client, request);
                 os.writeShort(response.length);
                 os.write(response);
+            } catch (final WireParseException e) {
+                LOGGER.info(e.getMessage());
             } catch (final Exception e) {
                 LOGGER.error(e.getMessage(), e);
             } finally {
