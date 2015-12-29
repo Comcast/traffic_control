40ddde7ab1ebe18a93e713b8c2c264243813b23d
David Neuman <david_neuman2@cable.comcast.com>
2015-12-29 09:00:37 -0700

    fixed test to work

--- traffic_router/core/src/test/java/com/comcast/cdn/traffic_control/traffic_router/core/loc/GeoLocationIP2Test.java
+++ traffic_router/core/src/test/java/com/comcast/cdn/traffic_control/traffic_router/core/loc/GeoLocationIP2Test.java
@@ -17,17 +17,19 @@
 package com.comcast.cdn.traffic_control.traffic_router.core.loc;
 
 import com.comcast.cdn.traffic_control.traffic_router.core.TestBase;
-import org.junit.After;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.comcast.cdn.traffic_control.traffic_router.core.loc.GeolocationException;
-import com.comcast.cdn.traffic_control.traffic_router.core.loc.MaxmindGeolocationService;
 import org.springframework.context.ApplicationContext;
 
 public class GeoLocationIP2Test {
-	private GeolocationService geoip2;
+	private static final Logger LOGGER = Logger.getLogger(GeoLocationIP2Test.class);
+
+	private GeolocationDatabaseUpdater geolocationDatabaseUpdater;
+	private GeolocationService geolocationService;
+	private NetworkUpdater networkUpdater;
 	private static ApplicationContext context;
 
 	@BeforeClass
@@ -41,25 +43,33 @@ public class GeoLocationIP2Test {
 
 	@Before
 	public void setUp() throws Exception {
-		geoip2 = (GeolocationService) context.getBean("GeolocationService");
+		geolocationDatabaseUpdater = (GeolocationDatabaseUpdater) context.getBean("geolocationDatabaseUpdater");
+		networkUpdater = (NetworkUpdater) context.getBean("networkUpdater");
+		geolocationService = (GeolocationService) context.getBean("GeolocationService");
 
+		while (!networkUpdater.isLoaded()) {
+			LOGGER.info("Network Updater is not loaded, waiting for a valid location database before proceeding");
+			Thread.sleep(1000);
+		}
+
+		while (!geolocationDatabaseUpdater.isLoaded()) {
+			LOGGER.info("GeoLocationDatabaseUpdater is not loaded, waiting for a valid location database before proceeding");
+			Thread.sleep(1000);
+		}
 	}
-	
+
 	@Test
-	public void testSerialLookupPerformance() throws GeolocationException {
+	public void testGeoLookupPerformance() throws GeolocationException {
 		long start = System.currentTimeMillis();
 		int total = 100000;
 
 		for (int i = 0; i <= total; i++) {
-			geoip2.location("10.0.0.1");
+			geolocationService.location("10.0.0.1");
 		}
 
 		long duration = System.currentTimeMillis() - start;
 		double tps = (double) total / ((double) duration / 1000);
-		System.out.println(geoip2.getClass().getName() + " lookup duration: " + duration + "ms, " + tps + " tps");
-	}
 
-	@After
-	public void tearDown() throws Exception {
+		System.out.println(geolocationService.getClass().getName() + " lookup duration: " + duration + "ms, " + tps + " tps");
 	}
 }
