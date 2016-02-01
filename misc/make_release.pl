6098aa8df2ecfb191adc9f4f596816882c1971f6
Dewayne Richardson <dewrich@gmail.com>
2016-02-01 15:49:41 -0700

    added more functionality

--- misc/make_release.pl
+++ misc/make_release.pl
@@ -23,6 +23,7 @@ use DBI;
 use Cwd;
 use Data::Dumper;
 use File::Find::Rule;
+use File::Path qw(make_path remove_tree);
 
 my $usage = "\n"
 	. "Usage:  $PROGRAM_NAME [--gpg-key [your-signed-key-id] --release_no=[release-to-create]\t\n\n"
@@ -30,18 +31,20 @@ my $usage = "\n"
 	. "Purpose:  This script automates the release process for the Traffic Control cdn.\n"
 	. "          defined in the dbconf.yml, as well as the database names.\n\n"
 	. "Flags:   \n\n"
-	. "--gpg-key  - Your gpg-key id. ie: 774ACED1\n"
-	. "--release-no   - The release_no name you want to cut. ie: 1.1.0\n"
-	. "--git-hash   - The git hash that will be used to reference the release. ie: da4aab57d \n"
-	. "--dry-run      - Simulation mode which will NOT apply any changes. \n"
+	. "--gpg-key          - Your gpg-key id. ie: 774ACED1\n"
+	. "--release-no       - The release_no name you want to cut. ie: 1.1.0\n"
+	. "--git-hash         - The git hash that will be used to reference the release. ie: da4aab57d \n"
+	. "--git-remote-url   - (optional) Git URL where the release will be sent (mostly for testing). ie: git\@github.com:yourrepo/traffic_control.git \n"
+	. "--dry-run          - (optional) Simulation mode which will NOT apply any changes. \n"
 	. "\nArguments:   \n\n"
-	. "release    - Cut the release, tag the release then make the branch, tag public\n"
+	. "release    - Cut the release, tag the release then make the branch, tag public.\n"
+	. "cleanup    - Reverses the release steps in case you messed up.\n"
 	. "pushdoc    - Upload documentation to the public website.\n";
 
 my $git_remote_name = 'official';
 
-my $official_git_remote_url = 'git@github.com:Comcast/traffic_control.git';
-my $git_remote_url          = 'git@github.com:Comcast/traffic_control.git';
+#my $git_remote_url = 'git@github.com:Comcast/traffic_control.git';
+my $git_remote_url = 'git@github.com:comcast/traffic_control.git';
 
 my $gpg_key;
 my $release_no;
@@ -58,8 +61,15 @@ my $new_branch;
 # Example: 774ACED1
 my $git_hash;
 my $dry_run = 0;
+my $working_dir;
 
-GetOptions( "gpg-key=s" => \$gpg_key, "release-no=s" => \$release_no, "git-hash=s" => \$git_hash, "dry-run!" => \$dry_run );
+GetOptions(
+	"gpg-key=s"        => \$gpg_key,
+	"release-no=s"     => \$release_no,
+	"git-hash=s"       => \$git_hash,
+	"git-remote-url=s" => \$git_remote_url,
+	"dry-run!"         => \$dry_run
+);
 
 STDERR->autoflush(1);
 my $argument = shift(@ARGV);
@@ -67,16 +77,16 @@ my $argument = shift(@ARGV);
 if ( defined($argument) ) {
 	parse_variables();
 
-	print "Updating 'VERSION' file\n";
-	my $cmd = "git clone " . $official_git_remote_url;
-	my $rc  = run_command($cmd);
-	if ( $rc > 0 ) {
-		print "Failed to run:" . $cmd . "\n";
-		exit(1);
-	}
-
 	if ( $argument eq 'release' ) {
-		cut_release_branch();
+		clone_repo_to_tmp();
+		cut_release();
+	}
+	elsif ( $argument eq 'cleanup' ) {
+		my $prompt = "Are you sure you want to cleanup release: " . $version . " this is irreversible";
+		if ( prompt_yn($prompt) ) {
+			clone_repo_to_tmp();
+			cleanup_release();
+		}
 	}
 	elsif ( $argument eq 'pushdoc' ) {
 		push_documentation();
@@ -91,13 +101,32 @@ else {
 
 exit(0);
 
+sub clone_repo_to_tmp {
+	my $tmp_dir = "/tmp";
+	my $tc_dir  = "traffic_control";
+	$working_dir = sprintf( "%s/%s", $tmp_dir, $tc_dir );
+	remove_tree($working_dir);
+	chdir $tmp_dir;
+	print " Updating 'VERSION' file \n ";
+	my $cmd = "git clone " . $git_remote_url;
+	chdir $working_dir;
+
+	my $rc = run_command($cmd);
+	if ( $rc > 0 ) {
+		print " Failed to run : " . $cmd . " \n ";
+		exit(1);
+	}
+
+}
+
 sub parse_variables {
 	my ( $major, $minor, $patch, $build_no ) = ( $release_no =~ /RELEASE-(\d).(\d).(\d)-(.*)/ );
-	print "release_no #-> (" . $release_no . ")\n";
+	print " release_no    #-> (" . $release_no . ")\n";
 	print "major #-> (" . $major . ")\n";
 	print "minor #-> (" . $minor . ")\n";
 	print "patch #-> (" . $patch . ")\n";
 	print "build_no #-> (" . $build_no . ")\n";
+	print "git_remote_url #-> (" . $git_remote_url . ")\n";
 	$version = sprintf( "%s.%s.%s", $major, $minor, $patch );
 
 	my $next_minor = $minor + 1;
@@ -107,10 +136,13 @@ sub parse_variables {
 	print "version #-> (" . $version . ")\n";
 	$new_branch = sprintf( "%s.%s.X", $major, $minor );
 	print "new_branch #-> (" . $new_branch . ")\n";
+
+	#TODO: drichardson - add validation logic here for the args
 }
 
-sub cut_release_branch {
+sub cut_release {
 
+	chdir $working_dir;
 	print "gpg_key #-> (" . $gpg_key . ")\n";
 	print "release_no #-> (" . $release_no . ")\n";
 	print "dry_run #-> (" . $dry_run . ")\n";
@@ -123,7 +155,7 @@ sub cut_release_branch {
 		print "Found Official : " . $git_remote_name . " " . $git_remote_url . "\n\n";
 	}
 
-	update_version_file();
+	update_version_file($next_version);
 	$cmd = "git commit -m 'Incrementing VERSION file' VERSION";
 	$rc  = run_command($cmd);
 	if ( $rc > 0 ) {
@@ -147,7 +179,8 @@ sub cut_release_branch {
 	}
 
 	print "Signing new tag based upon your gpg key\n";
-	$cmd = sprintf( "git tag -s -u %s -m '%s' %s", $git_hash, "Release 1.1.0 RC0", $version );
+	my $comment = "Release " . $version;
+	$cmd = sprintf( "git tag -s -u %s -m '%s' %s", $gpg_key, $comment, $version );
 	$rc = run_command($cmd);
 	if ( $rc > 0 ) {
 		print "Failed to run:" . $cmd . "\n";
@@ -161,16 +194,68 @@ sub cut_release_branch {
 		print "Failed to run:" . $cmd . "\n";
 		exit(1);
 	}
+}
 
-	#if ( $rc > 0 ) {
-	#die " System $cmd failed : $? ";
-	#}
+sub cleanup_release {
 
-	#print( " rc    #-> " . $rc . "\n" );
+	chdir $working_dir;
+	print "gpg_key #-> (" . $gpg_key . ")\n";
+	print "release_no #-> (" . $release_no . ")\n";
+	print "dry_run #-> (" . $dry_run . ")\n";
+	my $cmd = "git remote add official " . $git_remote_url;
+	my $rc  = run_command($cmd);
+	if ( $rc > 0 ) {
+		print "Added new origin: " . $git_remote_name . " " . $git_remote_url . "\n\n";
+	}
+	else {
+		print "Found Official : " . $git_remote_name . " " . $git_remote_url . "\n\n";
+	}
+
+	update_version_file($version);
+	$cmd = "git commit -m 'Decrementing VERSION file' VERSION";
+	$rc  = run_command($cmd);
+	if ( $rc > 0 ) {
+		print "Failed to run:" . $cmd . "\n";
+	}
+
+	print "Updating 'VERSION' file\n";
+	$cmd = "git push official master";
+	$rc  = run_command($cmd);
+	if ( $rc > 0 ) {
+		print "Failed to run:" . $cmd . "\n";
+		exit(1);
+	}
+
+	print "Creating new branch\n";
+	$cmd = "git push origin --delete " . $new_branch;
+	$rc  = run_command($cmd);
+	if ( $rc > 0 ) {
+		print "Failed to run:" . $cmd . "\n";
+		exit(1);
+	}
+
+	print "Removing old tag locally\n";
+	my $comment = "Release " . $version;
+	$cmd = sprintf( "git tag -d %s", $version );
+	$rc = run_command($cmd);
+	if ( $rc > 0 ) {
+		print "Failed to run:" . $cmd . "\n";
+		exit(1);
+	}
+
+	print "Removing old tag from remote\n";
+	$cmd = sprintf( "git push origin :refs/tags/%s", $version );
+	$rc = run_command($cmd);
+	if ( $rc > 0 ) {
+		print "Failed to run:" . $cmd . "\n";
+		exit(1);
+	}
 
 }
 
 sub update_version_file {
+
+	my $version_no = shift;
 
 	my $version_file_name = "VERSION";
 	open my $fh, '<', $version_file_name or die "error opening $version_file_name $!";
@@ -178,18 +263,32 @@ sub update_version_file {
 	print "Prior VERSION: " . $data . "\n";
 
 	if ($dry_run) {
-		print "Would have updated VERSION file to: " . $next_version . "\n";
+		print "Would have updated VERSION file to: " . $version_no . "\n";
 	}
 	else {
-		print "Updated VERSION file to: " . $next_version . "\n";
+		print "Updated VERSION file to: " . $version_no . "\n";
 		open( $fh, '>', $version_file_name ) or die "Could not open file '$version_file_name' $!";
-		print VERSION_FILE $next_version . "\n";
-		close VERSION_FILE;
+		print $fh $version_no . "\n";
+		close $fh;
 	}
 }
 
 sub deploy_documentation {
 
+}
+
+sub prompt {
+	my ($query) = @_;    # take a prompt string as argument
+	local $| = 1;        # activate autoflush to immediately show the prompt
+	print $query;
+	chomp( my $answer = <STDIN> );
+	return $answer;
+}
+
+sub prompt_yn {
+	my ($query) = @_;
+	my $answer = prompt("$query (Y/N): ");
+	return lc($answer) eq 'y';
 }
 
 sub run_command {
