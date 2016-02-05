29e94fbdbd06138e700d4eda2894f4e7507ce76a
Dewayne Richardson <dewrich@gmail.com>
2016-02-05 13:38:16 -0700

    successfully tested RC flow

--- misc/release.pl
+++ misc/release.pl
@@ -26,12 +26,12 @@ use File::Find::Rule;
 use File::Path qw(make_path remove_tree);
 
 my $usage = "\n"
-	. "Usage:  $PROGRAM_NAME --gpg-key=[your-signed-key-id] --release_no=[release-to-create]\t\n\n"
+	. "Usage:  $PROGRAM_NAME --gpg-key=[your-signed-key-id] --release-no=[release-to-create]\t\n\n"
 	. "Example:  $PROGRAM_NAME --gpg-key=75AFDE1 --release-no=RELEASE-1.1.0 \n\n"
 	. "Purpose:  This script automates the release process for the Traffic Control cdn.\n"
 	. "\nFlags:   \n\n"
 	. "--gpg-key          - Your gpg-key id. ie: 774ACED1\n"
-	. "--release-no       - The release_no name you want to cut. ie: 1.1.0\n"
+	. "--release-no       - The release-no name you want to cut. ie: 1.1.0\n"
 	. "--git-hash         - (optional) The git hash that will be used to reference the release. ie: da4aab57d \n"
 	. "--git-remote-url   - (optional) Overrides the git repo URL where the release will be pulled and sent (mostly for testing). ie: git\@github.com:yourrepo/traffic_control.git \n"
 	. "--dry-run          - (optional) Simulation mode which will NOT apply any changes. \n"
@@ -57,11 +57,17 @@ my $next_version;
 # Example: 1.1.x
 my $new_branch;
 
-# Example: 2377
+# Example: RC0
 my $build_no;
 
+# Example: RC1
+my $next_build_no;
+
 # Example: 774ACED1
-my $git_hash;
+my $git_short_hash;
+
+# Keeps track of the branch to determine RC flow
+my $branch_exists;
 
 my $rc;
 my $dry_run = 0;
@@ -70,7 +76,7 @@ my $working_dir;
 GetOptions(
 	"gpg-key=s"        => \$gpg_key,
 	"release-no=s"     => \$release_no,
-	"git-hash=s"       => \$git_hash,
+	"git-short-hash=s" => \$git_short_hash,
 	"git-remote-url=s" => \$git_remote_url,
 	"dry-run!"         => \$dry_run
 );
@@ -86,23 +92,33 @@ if ( defined($argument) ) {
 
 	if ( $argument eq 'branch' ) {
 		fetch_branch();
-		my $prompt = "Continue with the creating the release branch?";
+		my $prompt = "Continue with creating the RELEASE?";
 		if ( prompt_yn($prompt) ) {
-			cut_release();
+
+			print "branch_exists #-> (" . $branch_exists . ")\n";
+
+			# Only tag the release
+			if ($branch_exists) {
+				add_official_remote();
+				tag_and_push();
+			}
+			else {
+				add_official_remote();
+				cut_new_release();
+				tag_and_push();
+			}
+
 		}
 		else {
 			exit(0);
 		}
 	}
 	elsif ( $argument eq 'cleanup' ) {
-		my $prompt = "Are you sure you want to cleanup release: " . $version . " this is irreversible";
+		my $prompt = "\n\nAre you sure you want to cleanup the RELEASE? (" . $release_no . ")" . $version;
 		if ( prompt_yn($prompt) ) {
 			fetch_branch();
 			cleanup_release();
 		}
-	}
-	elsif ( $argument eq 'pushdoc' ) {
-		push_documentation();
 	}
 	else {
 		print $usage;
@@ -118,34 +134,68 @@ sub fetch_branch {
 
 	clone_repo_to_tmp();
 
-	if ( !defined($git_hash) ) {
-		( $rc, $git_hash ) = get_git_hash();
+	parse_variables();
+	chdir $working_dir;
+	( $rc, $branch_exists ) = check_branch_exists();
+
+	# if not passed in as option, then determine git hash
+	if ( !defined($git_short_hash) ) {
+		( $rc, $git_short_hash ) = get_git_short_hash();
 	}
 
-	parse_variables();
-
-	my $release_info = <<"INFO";
-\nRelease Info Summary
-Git Repo     : $git_remote_url
-Version      : $version
-Branch       : $new_branch
-Tag          : $release_no
-Git Hash     : $git_hash
-Next Version : $next_version
+	my $new_branch_info = <<"INFO";
+\nNEW Release Summary
+Git Repo       : $git_remote_url
+Version        : $version
+Branch         : $new_branch
+Tag            : $release_no
+Git Short Hash : $git_short_hash
+Next Version   : $next_version
 INFO
-	print $release_info;
+
+	my $release_candidate_info = <<"INFO";
+\nRelease CANDIDATE Summary
+Git Repo       : $git_remote_url
+Version        : $version
+Branch         : $new_branch
+Next Tag       : $release_no
+Git Hash       : $git_short_hash
+INFO
+	if ($branch_exists) {
+		print $release_candidate_info;
+	}
+	else {
+		print $new_branch_info;
+	}
 
 }
 
-sub get_git_hash {
+sub get_git_short_hash {
 	my $cmd = "git log --pretty=format:'%h' -n 1";
-	chdir $working_dir;
-	my ( $rc, $git_shorthash ) = run_and_capture_command( $cmd, "force" );
+	my ( $rc, $git_hash ) = run_and_capture_command( $cmd, "force" );
 	if ( $rc > 0 ) {
-		print " Failed to run : " . $cmd . " \n ";
-		exit(1);
+		print " Failed to retrieve git hash : " . $cmd . " \n ";
+
+		#exit(1);
 	}
-	return $rc, $git_shorthash;
+	return $rc, $git_hash;
+}
+
+sub check_branch_exists {
+	my $cmd = sprintf( "git checkout %s", $new_branch );
+	my $output;
+	my $git_branch;
+	( $rc, $output ) = run_and_capture_command( $cmd, "force" );
+
+	$cmd = sprintf( "git rev-parse --verify %s", $new_branch );
+	( $rc, $git_branch ) = run_and_capture_command( $cmd, "force" );
+	if ( $rc > 0 ) {
+		$branch_exists = 0;
+	}
+	else {
+		$branch_exists = 1;
+	}
+	return $rc, $branch_exists;
 }
 
 sub clone_repo_to_tmp {
@@ -160,10 +210,10 @@ sub clone_repo_to_tmp {
 
 	my $rc = run_command( $cmd, "force" );
 	if ( $rc > 0 ) {
-		print " Failed to run : " . $cmd . " \n ";
-		exit(1);
-	}
+		print " Failed to clone repo : " . $cmd . " \n ";
 
+		#exit(1);
+	}
 }
 
 sub parse_variables {
@@ -172,34 +222,34 @@ sub parse_variables {
 	my $patch;
 	( $major, $minor, $patch, $build_no ) = ( $release_no =~ /RELEASE-(\d).(\d).(\d)-(.*)/ );
 
+	my ( $rc_build_no, $x ) = ( $build_no =~ /RC(\d+)/ );
+
+	my $next_build_no_version = $rc_build_no + 1;
+	$next_build_no = sprintf( "RC%d", $next_build_no_version );
+
 	$version = sprintf( "%s.%s.%s", $major, $minor, $patch );
 
 	my $next_minor = $minor + 1;
 
 	$next_version = sprintf( "%s.%s.%s", $major, $next_minor, $patch );
 
-	$new_branch = sprintf( "%s.%s.X", $major, $minor );
+	$new_branch = sprintf( "%s.%s.x", $major, $minor );
 
 }
 
-sub cut_release {
-
-	chdir $working_dir;
-	print "gpg_key #-> (" . $gpg_key . ")\n";
-	print "release_no #-> (" . $release_no . ")\n";
-	print "dry_run #-> (" . $dry_run . ")\n";
+sub add_official_remote {
 	my $cmd = "git remote add official " . $git_remote_url;
 	my $rc  = run_command($cmd);
 	if ( $rc > 0 ) {
 		print "Added new origin: " . $git_remote_name . " " . $git_remote_url . "\n\n";
 	}
-	else {
-		print "Found Official : " . $git_remote_name . " " . $git_remote_url . "\n\n";
-	}
+}
+
+sub cut_new_release {
 
 	update_version_file($next_version);
-	$cmd = "git commit -m 'Incrementing VERSION file' VERSION";
-	$rc  = run_command($cmd);
+	my $cmd = "git commit -m 'Incrementing VERSION file' VERSION";
+	$rc = run_command($cmd);
 	if ( $rc > 0 ) {
 		print "Failed to run:" . $cmd . "\n";
 	}
@@ -208,39 +258,39 @@ sub cut_release {
 	$cmd = "git push official master";
 	$rc  = run_command($cmd);
 	if ( $rc > 0 ) {
-		print "Failed to run:" . $cmd . "\n";
-		exit(1);
+		print "Failed to push official to master" . $cmd . "\n";
 	}
 
 	print "Creating new branch\n";
 	$cmd = "git checkout -b " . $new_branch;
 	$rc  = run_command($cmd);
 	if ( $rc > 0 ) {
-		print "Failed to run:" . $cmd . "\n";
-		exit(1);
+		print "Failed to checkout new branch" . $cmd . "\n";
 	}
 
+}
+
+sub tag_and_push {
 	print "Signing new tag based upon your gpg key\n";
 	my $comment = "Release " . $version;
-	$cmd = sprintf( "git tag -s -u %s -m '%s' %s", $gpg_key, $comment, $release_no );
+	my $cmd = sprintf( "git tag -s -u %s -m '%s' %s", $gpg_key, $comment, $release_no );
 	$rc = run_command($cmd);
 	if ( $rc > 0 ) {
-		print "Failed to run:" . $cmd . "\n";
-		exit(1);
+		print "Failed to tag and push" . $cmd . "\n";
 	}
 
-	print "Making new tags and branch publicly available.\n";
+	print "Making new release tag and branch publicly available.\n";
+
+	#$cmd = "git push official " . $new_branch;
 	$cmd = "git push --follow-tags official " . $new_branch;
 	$rc  = run_command($cmd);
 	if ( $rc > 0 ) {
-		print "Failed to run:" . $cmd . "\n";
-		exit(1);
+		print "Failed to tag release" . $cmd . "\n";
 	}
 }
 
 sub cleanup_release {
 
-	chdir $working_dir;
 	print "gpg_key #-> (" . $gpg_key . ")\n";
 	print "release_no #-> (" . $release_no . ")\n";
 	print "dry_run #-> (" . $dry_run . ")\n";
@@ -265,7 +315,6 @@ sub cleanup_release {
 	$rc  = run_command($cmd);
 	if ( $rc > 0 ) {
 		print "Failed to run:" . $cmd . "\n";
-		exit(1);
 	}
 
 	print "Creating new branch\n";
@@ -273,24 +322,19 @@ sub cleanup_release {
 	$rc  = run_command($cmd);
 	if ( $rc > 0 ) {
 		print "Failed to run:" . $cmd . "\n";
-		exit(1);
 	}
 
-	print "Removing old tag locally\n";
 	my $comment = "Release " . $version;
-	$cmd = sprintf( "git tag -d %s", $version );
+	$cmd = sprintf( "git tag -d %s", $release_no );
 	$rc = run_command($cmd);
 	if ( $rc > 0 ) {
 		print "Failed to run:" . $cmd . "\n";
-		exit(1);
 	}
 
-	print "Removing old tag from remote\n";
-	$cmd = sprintf( "git push origin :refs/tags/%s", $version );
+	$cmd = sprintf( "git push origin :refs/tags/%s", $release_no );
 	$rc = run_command($cmd);
 	if ( $rc > 0 ) {
 		print "Failed to run:" . $cmd . "\n";
-		exit(1);
 	}
 
 }
@@ -299,17 +343,16 @@ sub update_version_file {
 
 	my $version_no = shift;
 
-	print " Updating 'VERSION' file \n ";
 	my $version_file_name = "VERSION";
 	open my $fh, '<', $version_file_name or die "error opening $version_file_name $!";
 	my $data = do { local $/; <$fh> };
-	print "Prior VERSION: " . $data . "\n";
 
 	if ($dry_run) {
 		print "Would have updated VERSION file to: " . $version_no . "\n";
 	}
 	else {
-		print "Updated VERSION file to: " . $version_no . "\n";
+		print "PRIOR version: " . $data . "\n";
+		print "NEXT  version: " . $version_no . "\n";
 		open( $fh, '>', $version_file_name ) or die "Could not open file '$version_file_name' $!";
 		print $fh $version_no . "\n";
 		close $fh;
@@ -343,6 +386,8 @@ sub run_and_capture_command {
 	else {
 		print "Capturing COMMAND> " . $cmd . "\n\n";
 		my $cmd_output = `$cmd </dev/null`;
+
+		#my $cmd_output = `$cmd >/dev/null 2>&1`;
 		return $?, $cmd_output;
 	}
 }
