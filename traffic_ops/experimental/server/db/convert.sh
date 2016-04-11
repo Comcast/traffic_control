45c508fd93a95679fabd021f43d6996e85ac220e
Dan Kirkwood <daniel_kirkwood@cable.comcast.com>
2016-04-11 09:00:40 -0600

    change region pkey

--- traffic_ops/experimental/server/db/convert.sh
+++ traffic_ops/experimental/server/db/convert.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
+#
+# Copyright 2016 Comcast Cable Communications Management, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
+# convert.sh uses nmig to migrate a traffic_ops 1.x mysql database to the start of a 2.x
+# postgres database.  It requires the nmig tool and node.js -- see
+# https://github.com/AnatolyUSS/nmig for installation instructions.
+#
+# Once this conversion is complete, the goose migration tool will be used to move
+# the database to the current migration level (see db/migrations/*).
 if [ ! -d "$GOPATH/src/github.com/AnatolyUSS/nmig" ]
 then
 	echo "Expecting to find '$GOPATH/src/github.com/AnatolyUSS/nmig'"
 	exit 1	
 fi
 
+# Modifications required on the mysql db for nmig to convert successfully: NOT NULL columns that have
+# empty strings in some entries will fail,  so we change them to allow NULL and postrgres db can be
+# updated later.
 read -rp 'Mysql user: ' user
 read -srp 'Password: ' pw
 mysql -u"$user" -p"$pw" to_development || (echo "mysql update failed"; exit 1) <<MYSQL_COMMANDS
@@ -24,8 +48,10 @@ alter table to_extension
    modify column script_file varchar(45) null;
 MYSQL_COMMANDS
 
+# TODO:  add username/passwd prompts for postgres here...
 dropdb to_development
 createdb to_development
 
+cd "$GOPATH/src/github.com/AnatolyUSS/nmig"
 rm -r logs_directory
 node --expose-gc main.js
---
+++ traffic_ops/experimental/server/db/migrations/20160000000000_region_pkey.sql
@@ -0,0 +1,21 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+ALTER TABLE phys_location
+	DROP CONSTRAINT phys_location_region_fkey,
+	ADD COLUMN region_name CHARACTER VARYING(45);
+UPDATE phys_location
+	SET region_name = region.name
+	FROM region
+	WHERE region.id = phys_location.region;
+ALTER TABLE region
+	DROP CONSTRAINT region_pkey,
+	ADD CONSTRAINT region_pkey PRIMARY KEY (name),
+	DROP COLUMN id;
+ALTER TABLE phys_location
+	ADD CONSTRAINT phys_location_region_fkey
+	FOREIGN KEY (phys_location_region_fkey) REFERENCES region(name);
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
